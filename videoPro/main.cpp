5b4df437ad480e7c7aab65d06dc47c8beb264c5f
lingLong <eternity@lingLongdeMacBook-Pro.local>
2016-04-22 10:22:35 +0800

    Initial Commit

---
+++ videoPro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  videoPro
+//
+//  Created by lingLong on 16/4/22.
+//  Copyright (c) 2016年 ling. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
